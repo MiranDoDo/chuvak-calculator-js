631f47688a72d82db945c53a34e1e2d41adb3532
MiranDoDo <dreadforum@atomicmail.io>
2026-05-16 09:03:49 -0400

    Add project files

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+
+// Получения значения и его прибавление к полю input
+
+function get_num(num) {
+
+    input = String(document.getElementById("input").textContent).length
+
+    if(input <= 18){
+
+        document.getElementById("input").textContent += num
+    }
+    else{
+        // ничего не делаем
+    }
+
+}
+
+
+// Получения результата
+
+function result() {
+
+    nums = document.getElementById("input").innerText
+
+    answer = eval(nums).toFixed(5)
+
+    if(answer == Infinity){
+        document.getElementById("input").textContent = "На 0 делить нельзя!"
+        
+    }
+    else{
+        document.getElementById("input").textContent = answer
+    }
+}
